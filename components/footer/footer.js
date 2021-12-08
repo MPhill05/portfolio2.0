1fe952872a783b43ca6fe52761c5849f3d43cdf9
Mark Phillips <mphill05@gmail.com>
2021-12-07 19:21:15 -0600

    basic footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,8 +1,25 @@
+import {
+  FooterContainer,
+  FooterWrap,
+  WebsiteRights
+} from './footer.styles';
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterContainer>
+      <FooterWrap>
+        <WebsiteRights>
+          <a
+            href='https://phillstack.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            aria-label='Github'
+            className="text-base font-normal text-gray-600 dark:text-gray-300"
+          >Mark Phillips
+          </a> &copy; {new Date().getFullYear()}. All rights reserved.
+        </WebsiteRights>
+      </FooterWrap>
+    </FooterContainer>
   )
 }
 
