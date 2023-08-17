fa4dd3d6f8258adf8f3e44b81ff5ce81d70b884c
Mark <mphill05@gmail.com>
2023-08-17 02:33:49 -0500

    code cleanup

--- components/blogcomponents/PostDetail.jsx
+++ components/blogcomponents/PostDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 import moment from 'moment';
-import CodeBlock from '../CodeBlock';
 import { RichText } from '@graphcms/rich-text-react-renderer';
 import styles from './PostDetail.module.scss';
 
@@ -89,7 +88,7 @@ const PostDetail = ({ post }) => {
           </div>
           <div className='font-medium text-gray-400'>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
             <span>
               {moment(post.createdAt).format('MMM DD, YYYY')}
